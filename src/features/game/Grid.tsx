cce83a41f0008a16ce0dc094b6f6731acba5df82
mrhanna <hanna.michaelr@gmail.com>
2025-07-02 15:58:50 -0500

    Relocated shift event handling to useShiftHandlers. Swipes and keypresses are throttled to 150ms.

--- src/features/game/Grid.tsx
+++ src/features/game/Grid.tsx
@@ -1,12 +1,7 @@
-import { useEffect } from 'react';
 import Tile, { type TileProps } from './Tile';
 import styled from 'styled-components';
-import { shift } from './gameActions';
-import type { Direction } from './gridLogic';
-import { useSwipeable } from 'react-swipeable';
-
 import type { GridState } from './gameReducer';
-import { useAppDispatch } from '../../app/AppContext';
+import useShiftHandlers from './useShiftHandlers';
 
 const GRID_SIZE = 4;
 
@@ -21,58 +16,28 @@ const GridView = styled.div`
 `
 
 export interface GridProps {
-    grid: GridState, 
+    grid: GridState,
 }
 
 export default function Grid({ grid }: GridProps) {
-    const dispatch = useAppDispatch();
     const tiles: TileProps[] = [];
 
-    const swipeHandlers = useSwipeable({
-        onSwipedLeft: () => { dispatch(shift('left')) },
-        onSwipedRight: () => { dispatch(shift('right')) },
-        onSwipedDown: () => { dispatch(shift('down')) },
-        onSwipedUp: () => { dispatch(shift('up')) },
-        preventScrollOnSwipe: true,
-        trackMouse: true,
-    })
-
-    useEffect(() => {
-        const keymap = {
-            ArrowUp: 'up',
-            ArrowDown: 'down',
-            ArrowLeft: 'left',
-            ArrowRight: 'right',
-        };
-
-        const handleKeyPress = (event: KeyboardEvent) => {  
-            const direction = keymap[event.code as keyof typeof keymap] as Direction | undefined;
-            if (direction) {
-                dispatch(shift(direction));
-            }
-        };
-
-        document.addEventListener('keydown', handleKeyPress)
-
-        return () => {
-            document.removeEventListener('keydown', handleKeyPress);
-        };
-    }, [])
+    const handlers = useShiftHandlers(grid);
 
     for (let row = 0; row < GRID_SIZE; row++) {
         for (let col = 0; col < GRID_SIZE; col++) {
             const cell = grid[row][col]
             if (cell) {
-                tiles.push({...cell.tile, $position: [row, col], merged: !!cell.merged});
+                tiles.push({ ...cell.tile, $position: [row, col], merged: !!cell.merged });
 
                 if (cell.merged) {
-                    tiles.push({...cell.merged, $position: [row, col], $exiting: true });
+                    tiles.push({ ...cell.merged, $position: [row, col], $exiting: true });
                 }
             }
         }
     }
 
-    // Sorting the tiles array helps prevents 
+    // Sorting the tiles array prevents unnecessary tile remounts
     tiles.sort((a, b) => {
         if (a.$id > b.$id) return 1;
         if (a.$id < b.$id) return -1;
@@ -80,8 +45,8 @@ export default function Grid({ grid }: GridProps) {
     });
 
     return (
-        <GridView {...swipeHandlers}>
-            <div style={{position: 'relative', width: '100%', height: '100%'}}>
+        <GridView {...handlers}>
+            <div style={{ position: 'relative', width: '100%', height: '100%' }}>
                 {tiles.map((props) => <Tile key={props.$id} {...props} />)}
             </div>
         </GridView>
---
+++ src/features/game/useShiftHandlers.ts
@@ -0,0 +1,67 @@
+import { useCallback, useEffect, useRef } from "react";
+import { simulateGridShift, type Direction } from "./gridLogic";
+import type { GridState } from "./gameReducer";
+import { useAppDispatch } from "../../app/AppContext";
+import { shift } from "./gameActions";
+import { useSwipeable } from "react-swipeable";
+
+export default function useShiftHandlers(grid: GridState) {
+    const dispatch = useAppDispatch();
+
+    // ref-ing the grid state in order to keep event handlers stable across rerenders
+    const canShiftRef = useRef(true);
+    const gridRef = useRef(grid);
+
+    useEffect(() => {
+        gridRef.current = grid;
+    }, [grid]);
+
+    const handleShiftEventWithThrottle = useCallback((direction: Direction) => {
+        if (canShiftRef.current) {
+            // preflighting the shift logic to determine whether to throttle key presses
+            const { changed } = simulateGridShift(gridRef.current, direction);
+
+            if (changed) {
+                canShiftRef.current = false;
+                dispatch(shift(direction));
+
+                setTimeout(() => {
+                    canShiftRef.current = true;
+                }, 150);
+            }
+        }
+    }, [dispatch])
+
+    const swipeHandlers = useSwipeable({
+        onSwipedLeft: () => { handleShiftEventWithThrottle('left') },
+        onSwipedRight: () => { handleShiftEventWithThrottle('right') },
+        onSwipedDown: () => { handleShiftEventWithThrottle('down') },
+        onSwipedUp: () => { handleShiftEventWithThrottle('up') },
+        preventScrollOnSwipe: true,
+        trackMouse: true,
+    })
+
+    useEffect(() => {
+        const keymap = {
+            ArrowUp: 'up',
+            ArrowDown: 'down',
+            ArrowLeft: 'left',
+            ArrowRight: 'right',
+        };
+
+        const handleKeyPress = (event: KeyboardEvent) => {
+            const direction = keymap[event.code as keyof typeof keymap] as Direction | undefined;
+            if (direction) {
+                handleShiftEventWithThrottle(direction);
+            }
+        };
+
+        document.addEventListener('keydown', handleKeyPress)
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [handleShiftEventWithThrottle]);
+
+    return swipeHandlers;
+}
