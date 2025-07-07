bfd09c98e118532f19681ff1b0ab6782b46a3a09
Michael Hanna <hanna.michaelr@gmail.com>
2025-07-07 12:26:19 -0500

    Prevented selection of text on tiles

--- src/features/game/Tile.tsx
+++ src/features/game/Tile.tsx
@@ -58,6 +58,11 @@ export const TileView = styled.div<{ exponent: number }>`
     justify-content: center;
     align-items: center;
 
+    -webkit-user-select: none; /* Safari */        
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* IE10+/Edge */
+    user-select: none; /* Standard */
+
     font-weight:bold;
 
     transition: color .3s, background-color .3s;
