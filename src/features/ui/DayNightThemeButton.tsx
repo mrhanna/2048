642e14d89aee41da6ddec69f4b317b6c58cf55fc
mrhanna <hanna.michaelr@gmail.com>
2025-07-11 14:53:29 -0500

    animated the night mode toggle

--- src/features/ui/DayNightThemeButton.tsx
+++ src/features/ui/DayNightThemeButton.tsx
@@ -4,30 +4,97 @@ import { useAppDispatch, useAppState } from "../../app/AppContext";
 import config from "../../app/config";
 import { themeChanged } from "./uiActions";
 import type { ComponentProps } from "react";
+import styled, { css, keyframes } from "styled-components";
+
+const iconEntrance = keyframes`
+    from {
+        transform: rotate(90deg);
+        opacity: 0;
+    }
+
+    50% {
+        opacity: 0;
+    }
+
+    to {
+        transform: rotate(0);
+        opacity: 1;
+    }
+`;
+
+const iconExit = keyframes`
+    from {
+        transform: rotate(0);
+        opacity: 1;
+    }
+
+    50% {
+        opacity: 0;
+    }
+
+    to {
+        transform: rotate(-90deg);
+        opacity: 0;
+    }
+`;
+
+const iconCss = css`
+    transform-origin: 0 300%;
+    grid-row: 1;
+    grid-column: 1;
+
+    &:not(.show) {
+        animation: ${iconExit} .6s ease-in;
+        opacity: 0;
+    }
+
+    &.show {
+        animation: ${iconEntrance} .6s ease-out;
+    }
+`
+
+const Sun = styled(HiSun)`
+    ${iconCss}
+`;
+
+const Moon = styled(HiMoon)`
+    ${iconCss}
+`;
+
+const DayNightStyledButton = styled(MenuButton)`
+    overflow: hidden;
+    display: grid;
+    place-items: center;
+`
 
 export default function DayNightThemeButton(props: ComponentProps<typeof MenuButton>) {
     const state = useAppState();
     const dispatch = useAppDispatch();
 
+    const themeName = state.ui.theme.name;
+
     const handleClick = () => {
         console.log(config.themes.default);
         console.log(config.themes.dark);
-        console.log(state.ui.theme.name === 'dark'
+        console.log(themeName === 'dark'
             ? config.themes.default
             : config.themes.dark);
         dispatch(themeChanged(
-            state.ui.theme.name === 'dark'
+            themeName === 'dark'
                 ? config.themes.default
                 : config.themes.dark
         ))
     }
 
     return (
-        <MenuButton onClick={handleClick} $variant="outline" {...props}>
-            {state.ui.theme.name === 'dark'
-                ? <HiMoon />
-                : <HiSun />
-            }
-        </MenuButton>
+        <DayNightStyledButton
+            onClick={handleClick}
+            $variant="outline"
+            aria-label="night mode toggle"
+            aria-pressed={themeName === 'dark'}
+            {...props}>
+            <Sun className={themeName !== 'dark' ? 'show' : undefined} />
+            <Moon className={themeName === 'dark' ? 'show' : undefined} />
+        </DayNightStyledButton>
     )
 }
