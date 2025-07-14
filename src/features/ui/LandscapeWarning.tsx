c033c2e817338b73d740698d621f78c06b991930
mrhanna <hanna.michaelr@gmail.com>
2025-07-14 13:01:53 -0500

    Landscape warning layout

--- src/features/ui/LandscapeWarning.tsx
+++ src/features/ui/LandscapeWarning.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const FullScreenWarning = styled.div`
     width: 100%;
-    height: 100%;
+    min-height: 80vh;
+    min-height: 80svh;
     display: grid;
     place-items: center;
 `
